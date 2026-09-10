e5cbfc4ff9b8970b01f1e53db38c75fbfe6177c7
rushabhthakurdware <rushabhthakudrware05@gmail.com>
2026-09-11 01:21:52 +0530

    Customize API welcome message

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());  // IMPORTANT
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Team Task Manager API is running"
+        message: "Welcome to Team Task Manager API"
     });
 });
 
